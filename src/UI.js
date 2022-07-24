41be66eba35e5a92ae717dd1870dfcc914797a65
CoreyAI <coreyiaquinta@gmail.com>
2022-07-24 04:45:29 -0400

    Fixed project list scanning method

--- src/UI.js
+++ src/UI.js
@@ -70,7 +70,7 @@ const ui = (() =>{
         </svg>
         <div class="nav-label">Projects</div>
       </div>
-      <div class="nav-projects" id="nav-added-projects"></div>
+      <div id="nav-added-projects"></div>
       <div class="nav-projects" id="nav-add-projects">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="nav-projects-icon">
           <path fill="#000000" d="M18 11H15V14H13V11H10V9H13V6H15V9H18M20 4V16H8V4H20M20 2H8C6.9 2 6 2.9 6 4V16C6 17.11 6.9 18 8 18H20C21.11 18 22 17.11 22 16V4C22 2.9 21.11 2 20 2M4 6H2V20C2 21.11 2.9 22 4 22H18V20H4V6Z" />
@@ -95,11 +95,11 @@ const ui = (() =>{
   const navAddProject = (name, id) => {
     const addedProjects = document.getElementById("nav-added-projects");
     const projectElement = document.createElement("div");
-    projectElement.setAttribute("class", "nav-projects");
-    projectElement.setAttribute("id", id);
+    // projectElement.setAttribute("class", "nav-projects");
+    projectElement.setAttribute("id", "testing123");
     projectElement.innerHTML = `
-      <div>${name}</div>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="nav-projects-icon">
+      <div class="nav-projects-test" id="${id}">${name}</div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="nav-projects-test" id="nav-projects-icon">
         <path fill="#000000" d="M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M7,13H17V11H7" />
       </svg>
     `;
--- src/index.js
+++ src/index.js
@@ -22,27 +22,40 @@ document.querySelector("#menu-icon").addEventListener("click", e => {
 
 let iProject = 0;
 document.querySelectorAll(".nav-projects").forEach(selection => {
+  console.log(selection);
   selection.addEventListener("click", function(e) {
-    console.log(this.id);
-    if (this.id == "nav-projects") {
-      ui.navProjectToggle();
-    } else if (this.id == "nav-add-projects") {
-      ui.navAddProject(`project#${iProject}`, iProject);
-      iProject++;
-    } else if (this.id == "nav-added-projects") {
-      console.log(e.target.id);
+    // console.log("this = ", this);
+    // console.log("e = ", e);
+    switch (this.id) {
+      case ("nav-projects"):
+        ui.navProjectToggle();
+        break;
+      case ("nav-add-projects"):
+        ui.navAddProject(`project#${iProject}`, iProject);
+        iProject++;
+        projectListScan();
+        break;
+      case ("nav-added-projects"):
+        // console.log(e.path[1].id);
+        console.log(this.id)
+        break;
+      default:
+        // break;
+        // console.log(e.path[1].id);
+        console.log(this.id);
+
     }
   });
 })
 
-//TODO: Fix click duplication bug!
 function projectListScan() {
-  let projects = document.querySelectorAll(".nav-added-projects");
-  projects.forEach(addedProject => {
-    addedProject.addEventListener("click", function(e) {
-      console.log(this);
-    });
-  });
+  const nodes = document.querySelectorAll(".nav-projects-test");
+  nodes.forEach(node => {
+    node.addEventListener("click", projectListEvent);
+  })
 }
 
-projectListScan();
+function projectListEvent(e) {
+  // console.log("project list event activated", e);
+  console.log(e);
+}
