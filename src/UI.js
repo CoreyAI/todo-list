aebafa45a66515213b7d6dd42634a668935e2715
CoreyAI <coreyiaquinta@gmail.com>
2022-07-24 06:32:13 -0400

    Added additional project controls

--- src/UI.js
+++ src/UI.js
@@ -98,15 +98,25 @@ const ui = (() =>{
     projectElement.setAttribute("class", "nav-added-projects");
     projectElement.setAttribute("id", id);
     projectElement.innerHTML = `
-      <div>${name}</div>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" id="nav-projects-icon">
+      <div class="project-name">${name}</div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="project-delete" id="nav-projects-icon">
         <path fill="#000000" d="M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M7,13H17V11H7" />
       </svg>
     `;
     addedProjects.appendChild(projectElement);
   }
 
-  return {start, menuToggle, navProjectToggle, navAddProject};
+  const showProjectContent = () => {
+    content.innerHTML = "TODO: project content";
+  }
+
+  const navRemoveProject = (element) => {
+    // const projects = document.getElementsByClassName("nav-added-projects");
+    // projects[index].remove();
+    element.remove();
+  }
+
+  return {start, menuToggle, navProjectToggle, navAddProject, showProjectContent, navRemoveProject};
 
 })();
 
