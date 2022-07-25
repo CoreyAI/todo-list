edfd3d8c614023ea556e27cf1f6ef2a9ea13eab9
CoreyAI <coreyiaquinta@gmail.com>
2022-07-25 03:06:46 -0400

    Initialize database objects

---
+++ src/db.js
@@ -0,0 +1,31 @@
+const projects = (name) => {
+  const taskList = [];
+  const getTasks = () => {
+    return taskList;
+  }
+  const setTask = (task) => {
+    taskList.push(task);
+  }
+  const removeTask = (taskName) => {
+    const taskIndex = taskList.indexOf(taskName);
+    taskList.splice(taskIndex, 1);
+  }
+  return {name, getTasks, setTask, removeTask};
+}
+
+const task = (title, description, dueDate, priority) => {
+  const status = false;
+  const getStatus = () => {
+    return status;
+  }
+  const setStatus = () => {
+    if (status == false) {
+      status = true;
+    } else {
+      status = false;
+    }
+  }
+  return {title, description, dueDate, priority, getStatus, setStatus};
+}
+
+export {projects, task};
