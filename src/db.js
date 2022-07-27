b04a0e3a794429d72a42c0bcf752d27aa92df215
CoreyAI <coreyiaquinta@gmail.com>
2022-07-27 05:19:53 -0400

    Added 'addProject' and 'removeProject' functions.

--- src/db.js
+++ src/db.js
@@ -1,3 +1,5 @@
+import * as content from "./content"
+
 const project = (name) => {
   const taskList = [];
   const getTasks = () => {
@@ -31,4 +33,35 @@ const task = (title, description, dueDate, priority) => {
 // Temporary location for storing projects
 let dbProject = [];
 
-export {project, task, dbProject};
+const addProject = (projectName) => {
+  const projectObject = project(projectName);
+  dbProject.push(projectObject);
+  content.showProject(projectName);
+}
+
+const removeProject = (projectName, activeView) => {
+  for (let i = 0; i < dbProject.length; i++) {
+    if (projectName == dbProject[i].name) {
+      dbProject.splice(i,1);
+      console.log("dbProject length: ", dbProject.length);
+      if (i > 0) {
+        if (activeView == projectName) {
+          content.showProject(dbProject[i-1].name);
+        } else {
+          return;
+        }
+      } else if ((i < 1) && (dbProject.length > 0)) {
+        if (activeView == projectName) {
+          content.showProject(dbProject[0].name);
+        }
+      } else {
+        // TODO: either show blank project placeholder or inbox/today tasks
+        content.resetContentContainer();
+      }
+      return;
+    }
+  }
+}
+
+// Consider removing dbProject from export and let the add/remove take over.
+export {project, task, dbProject, addProject, removeProject};
